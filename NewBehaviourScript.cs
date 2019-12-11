5d43452162acc7d47420c84bf78ebca8fcaed980
logos137 <55683692+logos137@users.noreply.github.com>
2019-12-11 16:16:33 -0700

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    public Transform [] target;
+    public float speed;
+
+    private int current;
+
+
+
+
+
+    // Update is called once per frame
+    void Update(){        
+        if (transform.position != target[current].position)
+            {
+                Vector3 pos = Vector3.MoveTowards(transform.position,target[current].position, speed * Time.deltaTime)
+                GetComponent<Ridgedbody>() MovePosition(pos);
+            }else current = (current +1) %target.Length
+        }
+        
+     }
+        
+    }
+}
